61c4f74c7fe06f41e13155091ad8b3c5ccd8b43c
solis_m <solis_m@ca01.ub.lan>
2026-03-02 15:59:40 +0800

    add 3 more trainers in t-s.s.ts

--- src/app/trainer-service.service.ts
+++ src/app/trainer-service.service.ts
@@ -15,6 +15,21 @@ private registry = signal([
     name:'Mitsy',
     team: ['Starmie', 'Psyduck', 'Gyarados'],
     items: ['Mystic Water','None', 'Gyaradosite']
+  },
+  {
+    name: 'Brock',
+    team: ['Onix', 'Geodude', 'Crobat'],
+    items: ['Hard Stone', 'Everstone', 'Poison Barb']
+  },
+  {
+    name: 'Sabrina',
+    team: ['Alakazam', 'Mr. Mime', 'Espeon'],
+    items: ['Twisted Spoon', 'Light Clay', 'Focus Sash']
+  },
+  {
+    name: 'Gary Oak',
+    team: ['Blastoise', 'Arcanine', 'Umbreon'],
+    items: ['Blastoisinite', 'Charcoal', 'Black Glasses']
   }
 ]);
 trainers = this.registry.asReadonly();
